b3bc9cb7306af6b9c18f466da5b86776e7335077
hanna7582 <hanna7582@naver.com>
2020-05-04 08:48:43 +0900

    Deploy

---
+++ assets/js/loginInfo.js
@@ -0,0 +1,31 @@
+var loginInfo = {
+  "grade": 0,
+  "level": 0,
+  "ctpCode": 0,
+  "ctpName": "전국",
+
+  "grade": 1,
+  "level": 1,
+  "ctpCode": 11,
+  "ctpName": "서울특별시",
+
+  "grade": 1,
+  "level": 1,
+  "ctpCode": 50,
+  "ctpName": "제주특별자치도",
+
+  "grade": 1,
+  "level": 2,
+  "ctpCode": 11,
+  "ctpName": "서울특별시 서초구",
+
+  "grade": 1,
+  "level": 3,
+  "ctpCode": 11,
+  "ctpName": "서울특별시 서초구 반포동",
+
+  "grade": 1,
+  "level": 3,
+  "ctpCode": 36,
+  "ctpName": "세종특별자치시 금남면",
+}
---
+++ assets/js/positionInfo.js
@@ -0,0 +1,94 @@
+const positionInfo = {
+  "전국": {
+    ctpCode: 0,
+    centroids: [35.87, 127.65],
+    zoom: 13
+  },
+  "서울특별시": {
+    ctpCode: 11,
+    centroids: [37.57, 126.98],
+    zoom: 9
+  },
+  "부산광역시": {
+    ctpCode: 26,
+    centroids: [35.18, 129.06],
+    zoom: 10
+  },
+  "대구광역시": {
+    ctpCode: 27,
+    centroids: [35.80, 128.55],
+    zoom: 10
+  },
+  "인천광역시": {
+    ctpCode: 28,
+    centroids: [37.49, 126.40],
+    zoom: 10
+  },
+  "광주광역시": {
+    ctpCode: 29,
+    centroids: [35.15, 126.83],
+    zoom: 9
+  },
+  "대전광역시": {
+    ctpCode: 30,
+    centroids: [36.33, 127.39],
+    zoom: 9
+  },
+  "울산광역시": {
+    ctpCode: 31,
+    centroids: [35.53, 129.23],
+    zoom: 10
+  },
+  "세종특별자치시": {
+    ctpCode: 36,
+    centroids: [36.56, 127.25],
+    zoom: 9
+  },
+  "경기도": {
+    ctpCode: 41,
+    centroids: [37.57, 127.10],
+    zoom: 11
+  },
+  "강원도": {
+    ctpCode: 42,
+    centroids: [37.71, 128.29],
+    zoom: 12
+  },
+  "충청북도": {
+    ctpCode: 43,
+    centroids: [36.60, 128.00],
+    zoom: 11
+  },
+  "충청남도": {
+    ctpCode: 44,
+    centroids: [36.52, 126.90],
+    zoom: 11
+  },
+  "전라북도": {
+    ctpCode: 45,
+    centroids: [35.72, 127.08],
+    zoom: 11
+  },
+  "전라남도": {
+    ctpCode: 46,
+    centroids: [34.80, 126.90],
+    zoom: 11
+  },
+  "경상북도": {
+    ctpCode: 47,
+    centroids: [36.31, 128.63],
+    zoom: 12
+  },
+  "경상남도": {
+    ctpCode: 48,
+    centroids: [35.20, 128.40],
+    zoom: 11
+  },
+  "제주특별자치도": {
+    ctpCode: 50,
+    centroids: [33.39, 126.56],
+    zoom: 10
+  }
+}
+
+export default positionInfo;
